8f119c6eb9e209a2999a7f7b78e48e30239684cd
rohit508 <lachmanrohit282@gmail.com>
2020-10-09 14:07:36 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var ON = document.getElementById("ON")
+var offbulb = document.getElementById("offbulb")
+
+function ONBULB() {
+    offbulb.src = "on.png"
+
+}
+
+function ONOFF() {
+    offbulb.src = "light-bulb-on-off.jpg"
+
+}
